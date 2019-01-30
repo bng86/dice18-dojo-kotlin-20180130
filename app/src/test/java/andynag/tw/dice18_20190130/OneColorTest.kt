5de4f0f0af4ec68c53f0905c7586ec56613428a3
andyang <yang.bng86@gmail.com>
2019-01-30 20:22:16 +0800

    Takuma

---
+++ app/src/test/java/andynag/tw/dice18_20190130/OneColorTest.kt
@@ -0,0 +1,16 @@
+package andynag.tw.dice18_20190130
+
+import org.junit.Test
+
+class OneColorTest {
+
+    @Test
+    fun test_one_color() {
+        val one = "[1,1,1,1]"
+        val oneArr = listOf(1, 1, 1, 1)
+        // convert one to array
+//        val converted = convertOneToArray()
+
+    }
+
+}
